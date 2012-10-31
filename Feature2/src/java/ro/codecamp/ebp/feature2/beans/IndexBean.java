515089103514e2a9936463fed094454149005ae6
George Alin Costea <costea@synygy.com>
2012-10-31 16:15:12 +0200

    Improved UI

--- Feature2/src/java/ro/codecamp/ebp/feature2/beans/IndexBean.java
+++ Feature2/src/java/ro/codecamp/ebp/feature2/beans/IndexBean.java
@@ -4,12 +4,14 @@ import javax.annotation.Resource;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 import ro.codecamp.ebp.core.api.modules.ModuleRegistryService;
+import ro.codecamp.ebp.core.api.modules.RegisteredModule;
 
 @Component
 @Scope("request")
 public class IndexBean 
 {   
-@Resource private ModuleRegistryService moduleRegistryService;
+    @Resource private ModuleRegistryService moduleRegistryService;
+    @Resource private RegisteredModule currentModule;
 
     public ModuleRegistryService getModuleRegistryService() 
     {
@@ -19,5 +21,15 @@ public class IndexBean
     public void setModuleRegistryService(ModuleRegistryService moduleRegistryService) 
     {
         this.moduleRegistryService = moduleRegistryService;
-    }  
+    } 
+
+    public RegisteredModule getCurrentModule() 
+    {
+        return currentModule;
+    }
+
+    public void setCurrentModule(RegisteredModule currentModule) 
+    {
+        this.currentModule = currentModule;
+    }    
 }
